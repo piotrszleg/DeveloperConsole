c6bf435703cf32f944d2f69b80261293fdafdc6c
piotrszleg <szlegp@gmail.com>
2020-09-29 21:19:16 +0200

    Console UI and scene.

---
+++ Assets/Console.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Console : MonoBehaviour
+{
+    public InputField input;
+    public Text text;
+
+    void Start()
+    {
+        input.onEndEdit.AddListener(TextSubmitted);
+    }
+
+    void TextSubmitted(string text) {
+        Write(text);
+        input.text = "";
+        input.ActivateInputField();
+    }
+
+    public void Write(string toWrite)
+    {
+        text.text += '\n' + toWrite;
+    }
+}
